57ce1074848c1fb84ceefaee8f4b2f9fcb7f7733
Houssein31 <hkharro95@gmail.com>
2023-10-03 22:27:51 -0400

    ArmstrongNumber and CalculateDistance added to FDM package

--- src/Neetcode150/linkedList/ListNode.java
+++ src/Neetcode150/linkedList/ListNode.java
@@ -12,4 +12,9 @@ public class ListNode {
             this.val = val; this.next = next;
       }
 
+      @Override
+      public String toString() {
+            if(next == null) return "{" +  val + "}";
+            return "{" +  val + "} -> " + next;
+      }
 }
